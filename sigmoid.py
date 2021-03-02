b2cda321d9670f3dd1136e196199fbb0855031fc
Nguyen Danh Nam <namnd2802@gmail.com>
2021-03-02 21:19:48 +0700

    Update

--- sigmoid.py
+++ sigmoid.py
@@ -4,15 +4,60 @@ from sklearn.datasets import load_iris
 
 
 def sigmoid(z):
+    """
+    sigmoid function
+    @param: z - vector
+    @type: array[float]
+    @return: vector
+    @rtype: array[float]
+    """
     return 1.0 / (1.0 + np.exp(-z))
 
 
 def cross_entropy(X, y, w):
+    """
+    return cross entropy cost function
+    @param:
+        X: matrix of data point
+        y: label of data
+        W: weight
+    @type:
+        X: narray[float]
+        y: narray
+        W: array
+    @return: cross entropy
+    @rtype: float
+    """
     y_hat = sigmoid(np.dot(X, w))
     return -np.sum(y * np.log(y_hat))
 
 
-def logistic_sigmoid(X, y, w_init, lr, tol=1e-5, max_iter=100000, regular=0.01):
+def logistic_sigmoid(X, y, w_init, lr, tol=1e-5, max_iter=100000, regular=0.0):
+    """
+    training sigmoid model with Stochastic Gradient Descent
+    @params:
+        X: matrix data train
+        y: label
+        w_init: weight init
+        lr: learning rate
+        tol:
+        max iter: maximum iteration
+        regurlar: regular coefficient
+    @type:
+        X: narray[float]
+        y: array
+        w_init: array[float]
+        lr: float
+        tol:float
+        max_iter: int
+        regular: float
+    @return
+        w: list of weight, final element is best weight
+        max_iter: number of iteration when model coverged
+    @rtype:
+        w: List[array]
+        max_iter: int
+    """
     w = [w_init]
     N = X.shape[0]
     it = 0

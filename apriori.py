03bcc8730819467d1de6170e13dce7fefde26a78
Thanh Tung Nguyen <65433836+tungstudies@users.noreply.github.com>
2021-03-01 20:59:26 -0500

    Add files via upload

--- apriori.py
+++ apriori.py
@@ -3,91 +3,157 @@ import pandas as pd
 from itertools import combinations
 
 
-def parse(file_name):
-    # doc data bang pandas
+def parse(file_name: str):
+    """
+    Convert input CSV data to a tuple of transaction list and dictionary of transaction items:
+
+    @param file_name: location of CSV data file
+    @type file_name: str
+    @return: transaction list: item words are converted to sequential index
+             transaction items: a dictionary mapping item word with its index
+    @rtype: Tuple[List[Set[int]], dict]
+    """
+
+    # read csv file with pandas
     data = pd.read_csv(file_name)
-    # bien X luu cac transaction
-    X = []
-    # items luu danh sach item
-    items = {}
-    # count - stt cua item
-    count = 0
-    for i in range(data.shape[0]):
-        # bien x luu cac item co trong mot transaction
-        x = []
-        for j in range(data.shape[1]):
-            # lay item thu j cua transaction i
+
+    # Variable transaction_list - to store transaction
+    transaction_list = []
+
+    # Use a dict to store items from the given transactions
+    items_dict = {}
+
+    # index_count - sequential index of each item
+    index_count = 0
+
+    # total number of transactions of the given data
+    total_transaction_number = data.shape[0]
+
+    # maximum number of items per transaction (max number of columns) of the given data
+    max_items_per_tranx = data.shape[1]
+
+    for i in range(total_transaction_number):
+
+        # items_each_trx to store items in each transaction
+        items_each_trx = []
+
+        for j in range(max_items_per_tranx):
+
+            # get item j (position) of transaction i
             item = data.iloc[i, j]
-            # neu nan hay null thi bo qua
+            # if item is NaN or Null -> skip the loop
             if str(item) == "nan":
                 continue
             else:
-                # neu item do co trong danh thi them vao transaction x
-                if item.lower().strip() in items:
-                    x.append(items[item.lower().strip()])
-                # neu item do khong co trong danh sach items thi them vao ds items va them vao transaction x
+                # if the item is in items_dict, to add its index to transaction_list
+                if item.lower().strip() in items_dict:
+                    items_each_trx.append(items_dict[item.lower().strip()])
+
+                # if the item is NOT in items_dict, to compute its index, add to items_dict and
+                # add its index to transaction_list
                 else:
-                    items[item.lower().strip()] = count
-                    x.append(count)
-                    count += 1
-        X.append(x)
-    return X, items
+                    items_dict[item.lower().strip()] = index_count
+                    items_each_trx.append(index_count)
+                    index_count += 1
+        transaction_list.append(items_each_trx)
+
+    return transaction_list, items_dict
 
 
-def gen_candidate(X):
-    c1 = []
-    # duyet qua tung transaction, tao ra cac item set co kich thuoc la 1
-    for transaction in X:
+def get_candidates(transaction_list: list):
+    """
+    Generate size-one-candidates (one-item sets) from the list of transactions
+    @param transaction_list: list of transactions with transaction items being coded by their sequential indexes
+    @type transaction_list: List[Set[int]]
+    @return: list of sets of size-one-candidate (one-item sets)
+    @rtype: List[set]
+    """
+
+    # size_one_candidates: a list to store size-one candidates (one-item sets)    
+    size_one_candidates = []
+    # loop through each transaction to create  item sets of size-one (one-item sets)
+    for transaction in transaction_list:
         for i in transaction:
             i = frozenset([i])
-            if i not in c1:
-                c1.append(i)
-    return c1
+            if i not in size_one_candidates:
+                size_one_candidates.append(i)
+    return size_one_candidates
 
 
-def gen_candidate_k(freq_item, k):
+def generate_candidate_k(freq_item: list, k: int) -> list:
     """
-    tao danh sach gom cack-itemset
-    """
-    ck = []
+    Generate list of  of size-k candidates from the  frequent item list (freq_item)
 
-    # tao ung vien co kich thuoc la 2 (2-itemset)
+    @param freq_item: list of qualified item sets (item sets with support >= min_support
+    @type freq_item:  list
+    @param k:
+    @type k:
+    @return:
+    @rtype:
+    """
+    size_k_candidates = []
+
+    # to generate candidates of size two (2-item sets)
+
     if k == 0:
+        # TODO: giải thích tại sao?
         for f1, f2 in combinations(freq_item, 2):
-            item = f1 | f2  # ket hop 2 item set
-            ck.append(item)
+            item = f1 | f2  # union of two sets)
+            size_k_candidates.append(item)
+
+    # k > 0
     else:
         for f1, f2 in combinations(freq_item, 2):
-            # neu 2 k+1 (item set) co k phan tu chung thi chung se hop nhat thanh ung cu vien k+2 itemset
+            # if the two (k+1)-item sets both has k common items
+            # then they will be combined to be the (k+2)-item candidate
+            # TODO: giải thích tại sao?
             intersection = f1 & f2
             if len(intersection) == k:
                 item = f1 | f2
-                if item not in ck:
-                    ck.append(item)
-    return ck
+                if item not in size_k_candidates:
+                    size_k_candidates.append(item)
+
+    return size_k_candidates
 
 
-def create_freq_item(X, ck, min_support):
+def create_freq_item(transaction_list, size_k_candidates, min_support):
     """
-    tim ra cac itemset co support >= min_support
+    find candidates whose support >= min_support
+    @param transaction_list: list of transactions with transaction items being coded by their sequential indexes
+    @type transaction_list: List[Set[int]]
+    @param size_k_candidates:
+    @type size_k_candidates:
+    @param min_support: The minimum fraction of transactions an itemets needs to occur in to be deemed frequent
+    @type min_support: float
+    @return:                    
+    @rtype:
+
     """
-    # lap lai tung transaction va tinh toan so luong tung item co xuat hien trong du lieu
+    # loop through the transaction and compute the count for each candidate (item)
     item_count = {}
-    for transaction in X:
-        for item in ck:
-            if item.issubset(transaction):
-                if item not in item_count:
-                    item_count[item] = 1
-                else:
-                    item_count[item] += 1
 
-    n_row = X.shape[0]
-    #freq_item tan suat xuat hien cua tung item set, luu danh sach item set xuat hien thuong xuyen
+    # check in each transaction
+    for transaction in transaction_list:
+
+        # to see if the items of each size-k candidate is part (subset) of the transaction
+        # if the candidate is the subset add to item_count dictionary together with its count
+        for candidate in size_k_candidates:
+            if candidate.issubset(transaction):
+                if candidate not in item_count:
+                    item_count[candidate] = 1
+                else:
+                    item_count[candidate] += 1
+
+    # total number of transactions
+    n_row = transaction_list.shape[0]
+
+    # freq_item indicates the frequency of occurence of each item set. To store the frequent item sets
     freq_item = []
-    # do support cua tung item set
+
+    # if the support of an item is greater than the min_support, then it is considered as frequent
+    # compute support of each item set , if >= min_support -> append that item set to freq_item
     item_support = {}
 
-    # tinh do support, neu >= min_support thi them no vao freq_item
     for item in item_count:
         support = item_count[item] / n_row
         if support >= min_support:
@@ -98,25 +164,42 @@ def create_freq_item(X, ck, min_support):
     return freq_item, item_support
 
 
-def apriori(X, min_support):
-    #gen 1-itemset
-    c1 = gen_candidate(X)
-    # gen freq_item tu 1-item
-    freq_item, item_support_dict = create_freq_item(X, c1, min_support=0.01)
-    # freq_items danh sach k-itemset
+def apriori(transaction_list, min_support=0.01):
+    """
+    @param transaction_list:
+    @type transaction_list:
+    @param min_support:
+    @type min_support:
+    @return:
+    @rtype:
+    """
+    # generate size-one-candidates (one-item sets) from the list of transactions
+    size_one_candidates = get_candidates(transaction_list)
+
+    # find candidates/item sets whose support >= min_support from size_one_candidates
+    freq_item, item_support_dict = create_freq_item(transaction_list, size_one_candidates, min_support=min_support)
+
+    # generate size-k candidates from the list of qualified qualified size-one item sets (aka freq_items)
     freq_items = [freq_item]
 
-    #duyet cac item set pho bien
+    # loop through freq_items and add list of k-incremental-size to it while looping
     k = 0
     while len(freq_items[k]) > 0:
-        #lay ra danh sach k - item set
+
+        # get list of size-k item sets
         freq_item = freq_items[k]
-        # gen ra k+2 itemset 
-        ck = gen_candidate_k(freq_item, k)
-        #tim cac k-itemset pho bien
-        freq_item, item_support = create_freq_item(X, ck, min_support=0.01)
+        
+        # generate k+2 itemset
+        size_k_candidates = generate_candidate_k(freq_item, k)
+
+        # find frequent item sets/candidates of k-size
+        freq_item, item_support = create_freq_item(transaction_list, size_k_candidates, min_support=min_support)
+
+        # append to the qualified frequent item set to freq_items and update their support
         freq_items.append(freq_item)
         item_support_dict.update(item_support)
+
+        # increment k and keep looping to add until exhausted
         k += 1
 
     return freq_items, item_support_dict
@@ -131,14 +214,14 @@ def compute_conf(freq_items, item_support_dict, freq_set, subsets, min_confidenc
 
     for rhs in subsets:
         # tao danh sach ben trai cua luat lien ket, them vao rules neu luat sinh ra > min_confidence
-        #lhs - danh sach item phia trai cua luat lien ket, rhs - danh sach item phia ben phai
+        # lhs - danh sach item phia trai cua luat lien ket, rhs - danh sach item phia ben phai
         lhs = freq_set - rhs
-        #tinh confidence cua luat
+        # tinh confidence cua luat
         conf = item_support_dict[freq_set] / item_support_dict[lhs]
-        #check neu luat du dieu kien >= min_conf
+        # check neu luat du dieu kien >= min_conf
         if conf >= min_confidence:
             lift = conf / item_support_dict[rhs]
-            #rules-luat lien ket
+            # rules-luat lien ket
             rules_info = lhs, rhs, conf, lift
             rules.append(rules_info)
             right_hand_side.append(rhs)
@@ -149,8 +232,8 @@ def compute_conf(freq_items, item_support_dict, freq_set, subsets, min_confidenc
 def create_rules(freq_items, item_support_dict, min_confidence):
     association_rules = []
 
-    #duyet qua tung danh sach cac k-item set pho bien
-    #tinh tu k = 2, do k =1 itemset chi co mot phan tu thi khong the tach thanh lhs va rhs
+    # duyet qua tung danh sach cac k-item set pho bien
+    # tinh tu k = 2, do k =1 itemset chi co mot phan tu thi khong the tach thanh lhs va rhs
     for idx, freq_item in enumerate(freq_items[1:(len(freq_items) - 1)]):
         for freq_set in freq_item:
 
@@ -159,11 +242,11 @@ def create_rules(freq_items, item_support_dict, min_confidence):
                 freq_items, item_support_dict, freq_set, subsets, min_confidence)
             association_rules.extend(rules)
 
-            #duyet qua cac itemset pho bien, size itemset tu k=3 tro len
+            # duyet qua cac itemset pho bien, size itemset tu k=3 tro len
             if idx != 0:
                 k = 0
                 while len(right_hand_side[0]) < len(freq_set) - 1:
-                    ck = gen_candidate_k(right_hand_side, k=k)
+                    ck = generate_candidate_k(right_hand_side, k=k)
                     rules, right_hand_side = compute_conf(freq_items, item_support_dict,
                                                           freq_set, ck, min_confidence)
                     association_rules.extend(rules)
@@ -173,7 +256,8 @@ def create_rules(freq_items, item_support_dict, min_confidence):
 
 
 if __name__ == "__main__":
-	X, items = parse("data/a1_market_basket_optimisation.csv")
-	X = np.array(X)
-	freq_items, item_support_dict = apriori(X, min_support = 0.01)
-	association_rules = create_rules(freq_items, item_support_dict, min_confidence=0.5)
+    X, items = parse("data/a1_market_sample.csv")
+    X = np.array(X)
+    freq_items, item_support_dict = apriori(X, min_support=0.01)
+    association_rules = create_rules(freq_items, item_support_dict, min_confidence=0.5)
+    print(association_rules[:5])
